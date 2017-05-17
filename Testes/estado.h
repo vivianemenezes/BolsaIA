9c831147ca4895b563240046059d3540ac38dcab
vivianemenezes <vivianemenezes@ufc.br>
2017-05-17 14:15:34 -0300

    Update estado.h

--- Testes/estado.h
+++ Testes/estado.h
@@ -7,8 +7,8 @@ using namespace std;
 
 struct estado{
     char nome;
-    list<estado> relacao;
-    list<char> preposicao;
+    list<estado> relacao; 
+    list<char> preposicao; //mudar para proposicoes
 
     estado(char nome){
         this->nome = nome;
